1c70b974cf356761d41eab02be891cf45e84221e
Anh Hai Trinh <anh.hai.trinh@gmail.com>
2010-01-08 22:05:47 +0700

    minor change

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 
-import os, sys
+import os
+import sys
+
 from distutils.core import setup
 
-sys.path.insert(0, os.path.dirname(__file__))
+__dir__ = os.path.realpath(os.path.dirname(__file__))
+
+sys.path.insert(0, __dir__)
 try:
     import stream
 finally:
@@ -23,10 +27,10 @@ setup(
 	name = 'stream',
 	version = stream.__version__,
 	description = stream.__doc__.split('\n')[0],
-  long_description = ''.join(open('stream.py').readlines()[2:116]),
+	long_description = ''.join(open('stream.py').readlines()[2:117]),
 	author = 'Anh Hai Trinh',
 	author_email = 'moc.liamg@hnirt.iah.hna:otliam'[::-1],
-  keywords='lazy iterable iterator generator stream data flow functional processing',
+	keywords='lazy iterator generator stream pipe parallel data flow functional list processing',
 	url = 'http://github.com/aht/stream.py',
 	platforms=['any'],
 	classifiers=filter(None, classifiers.split("\n")),

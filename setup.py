72415c287971748d53a98368c2b8e3adff6eeb48
Anh Hai Trinh <anh.hai.trinh@gmail.com>
2010-01-15 16:30:55 +0700

    Update setup.py for 0.8 release

--- setup.py
+++ setup.py
@@ -23,14 +23,24 @@ Topic :: Software Development :: Libraries :: Python Modules
 Topic :: Utilities
 """
 
+__doc__ = """Streams are iterables with a pipelining mechanism to enable data-flow
+programming and easy parallelization.
+
+See the reference documentation at <http://www.trinhhaianh.com/stream.py>.
+
+Articles written by the author about the module can be viewed at <http://blog.onideas.ws/tag/project:stream.py>.
+
+The code repository is located at <http://github.com/aht/stream.py>.
+"""
+
 setup(
 	name = 'stream',
 	version = stream.__version__,
 	description = stream.__doc__.split('\n')[0],
-	long_description = ''.join(open('stream.py').readlines()[2:100]),
+	long_description = __doc__,
 	author = 'Anh Hai Trinh',
 	author_email = 'moc.liamg@hnirt.iah.hna:otliam'[::-1],
-	keywords='lazy iterator generator stream pipe parallel data flow functional list processing',
+	keywords='lazy iterator generator stream pipe parallellization data flow functional list processing',
 	url = 'http://github.com/aht/stream.py',
 	platforms=['any'],
 	classifiers=filter(None, classifiers.split("\n")),
